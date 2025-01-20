9ec75cdaa6323e1f1b680af8bcd1bae84a063931
Dmytro Dybach <dd2008r@gmail.com>
2025-01-20 22:36:48 +0200

    final navbar

--- diaryproject/diaryapp/views.py
+++ diaryproject/diaryapp/views.py
@@ -1,25 +1,31 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import SchoolClass
 
 def login_view(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        # Важливо: якщо ви використовуєте email для входу, потрібно переконатися,
-        # що ви правильно налаштували AUTH_USER_MODEL або використовуєте правильне поле для автентифікації
         user = authenticate(request, username=email, password=password)
-        
+       
         if user is not None:
             login(request, user)
             messages.success(request, 'Successfully logged in!')
-            return redirect('index')  # переконайтеся, що у вас є URL з ім'ям 'index'
+            return redirect('index')
         else:
             messages.error(request, 'Invalid email or password!')
-    
-    return render(request, 'login.html')  # змініть на правильний шлях до вашого шаблону
+   
+    return render(request, 'login.html')
+
+@login_required
+def index(request):
+    school_classes = SchoolClass.objects.all() #navbar 'disabled'
+    context = {
+        'school_classes': school_classes
+    }
+    return render(request, 'index.html', context)
 
 @login_required
 def logout_view(request):
@@ -27,10 +33,8 @@ def logout_view(request):
     messages.info(request, 'Successfully logged out!')
     return redirect('login')
 
-@login_required
-def index(request):
-    return render(request, 'index.html')
 
 def logout_view(request):
     logout(request)
     return redirect('login')
+
